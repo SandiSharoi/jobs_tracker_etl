79355173d236b1d534353fc781ae1551afc6c7a3
Sandi Sharoi <sandisharoi18@gmail.com>
2025-05-23 01:20:57 +0630

    Add salary, min salary, max salary, currency, role and functions fields

--- utils/founditSG.py
+++ utils/founditSG.py
@@ -56,7 +56,8 @@ class FounditScraper:
                 "cloud computing",
                 "enterprise software",
                 "data center",
-                "cloud data services"
+                "cloud data services"                
+
             ],
 
       }
@@ -90,7 +91,7 @@ class FounditScraper:
 
             "industries", "roles", "jobTypes", "qualifications",
 
-            "companyId", "companyName", "salary", "seoCompanyUrl", "seoJdUrl"
+            "companyId", "companyName", "salary", "seoCompanyUrl", "seoJdUrl", "roles", "functions"
 
         ]
 
@@ -144,7 +145,8 @@ class FounditScraper:
                 time.sleep(1) #Sleep between requests
                 start += 15
 
-                if start >= 600: #Hard limit to break early for safety
+# start >= 600:
+                if start >= 300: #Hard limit to break early for safety
                     logging.info(" Reached start=600. Stopping to avoid scraping too much.")
                     break
 
