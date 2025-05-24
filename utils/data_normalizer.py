9859de5e47afef94268ed84de99b54efb62cb15a
Sandi Sharoi <sandisharoi18@gmail.com>
2025-05-24 19:41:36 +0630

    Add Category

--- utils/data_normalizer.py
+++ utils/data_normalizer.py
@@ -2,6 +2,8 @@ import pandas as pd
 from datetime import datetime, timedelta
 import pytz
 import numpy as np
+from utils.job_categorizer import categorize_job_role
+
 
 def parse_salary_founditSG(s):
     if not isinstance(s, str) or not s.strip():
@@ -27,7 +29,7 @@ class JobDataNormalizer:
 
     def __init__(self):
         self.standard_cols = [
-            'title', 'company', 'location', 'role', 'functions', 'salary',
+            'title', 'category', 'company', 'location', 'role', 'salary',
             'minimum_salary', 'maximum_salary', 'currency',
             'job_type', 'work_arrangement', 'date_posted',
             'job_link', 'country', 'source'
@@ -75,7 +77,6 @@ class JobDataNormalizer:
             'companyName': 'company',
             'locations': 'location',
             'roles': 'role',
-            'functions': 'functions',
             'salary': 'salary',
             'jobTypes': 'job_type',
             'postedBy': 'date_posted',
@@ -87,7 +88,6 @@ class JobDataNormalizer:
         df['role'] = df['role'].apply(lambda x: ', '.join(x) if isinstance(x, list) else x)
 
          # Convert job_type list to string
-        df['functions'] = df['functions'].apply(lambda x: ', '.join(x) if isinstance(x, list) else x)       
 
         # Convert job_type list to string
         df['job_type'] = df['job_type'].apply(lambda x: ', '.join(x) if isinstance(x, list) else x)
@@ -110,9 +110,91 @@ class JobDataNormalizer:
         df['work_arrangement'] = None
         df['source'] = 'founditsg'
 
+        # Role normalization using practical hybrid flow
+        df['category'] = df['role'].apply(categorize_job_role)
+
         # Apply missing value filler
         df = self.fill_missing_foundit_fields(df)
 
 
 
+        return df[self.standard_cols]
+
+
+    ## Jobnet Myanmar
+    def jobnetmm(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Normalize the job data from JobNet Myanmar.
+        """
+        df = df.rename(columns={
+            'Title': 'title',
+            'Company': 'company',
+            'Location': 'location',
+            'Salary': 'salary',
+            'Date_Posted': 'date_posted',
+            'Job_Link': 'job_link'
+        })
+        df['country'] = 'MM'
+        df['job_type'] = None
+        df['work_arrangement'] = None
+        df['source'] = 'jobnetmm'
+        return df[self.standard_cols]
+    
+    ## JobsDB Singapore
+    def jobsdbsg(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Normalize the job data from JobsDB Singapore.
+        """
+        df = df.rename(columns={
+            'Title': 'title',
+            'Company': 'company',
+            'Location': 'location',
+            'Salary': 'salary',
+            'Job_Type': 'job_type',
+            'Work_Arrangement': 'work_arrangement',
+            'Job_Link': 'job_link',
+            'Date_Posted': 'date_posted'
+        })
+        df['country'] = 'SG'
+        df['source'] = 'jobsdbsg'
+        return df[self.standard_cols]
+    
+    ## JobsDB Thailand
+    def jobsdbth(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Normalize the job data from JobsDB Thailand.
+        """
+        df = df.rename(columns={
+            'job_title': 'title',
+            'company': 'company',
+            'location': 'location',
+            'salary': 'salary',
+            'job_type': 'job_type',
+            'work_arrangement': 'work_arrangement',
+            'date_posted': 'date_posted',
+            'job_link': 'job_link'
+        })
+        df['country'] = df['country_code']
+        df['source'] = 'jobsdbth'
+        return df[self.standard_cols]
+    
+
+  
+    ## JobStreet Malaysia
+    def jobstreetmalay(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Normalize the job data from JobStreet Malaysia.
+        """
+        df = df.rename(columns={
+            'job_title': 'title',
+            'company': 'company',
+            'location': 'location',
+            'salary': 'salary',
+            'job_type': 'job_type',
+            'work_arrangement': 'work_arrangement',
+            'date_posted': 'date_posted',
+            'job_link': 'job_link'
+        })
+        df['country'] = df['country_code']
+        df['source'] = 'jobstreetmalay'
         return df[self.standard_cols]
